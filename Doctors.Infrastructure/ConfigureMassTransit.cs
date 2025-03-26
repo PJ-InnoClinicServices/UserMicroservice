f361e952f36bfff8881931c18785fbdd1ab34ed5
piotrjass <131187708+piotrjass@users.noreply.github.com>
2025-03-26 12:07:41 +0100

    Add mass transit

---
+++ Doctors.Infrastructure/ConfigureMassTransit.cs
@@ -0,0 +1,29 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Doctors.Infrastructure;
+public static class MassTransitExtensions
+{ 
+    public static IServiceCollection AddMassTransitServices(this IServiceCollection services)
+    {
+        services.AddMassTransit(x =>
+        {
+            x.AddConsumers(typeof(MassTransitExtensions).Assembly);
+            x.UsingRabbitMq((context, cfg) =>
+            {
+                cfg.Host("localhost", h =>
+                {
+                    h.Username("guest");
+                    h.Password("guest");
+                });
+        
+                cfg.ReceiveEndpoint("queue", e =>
+                {
+                    e.UseMessageRetry(r => r.Interval(3, TimeSpan.FromSeconds(0.2)));
+                    e.ConfigureConsumers(context);
+                });
+            });
+        });
+        return services;
+    }
+}
