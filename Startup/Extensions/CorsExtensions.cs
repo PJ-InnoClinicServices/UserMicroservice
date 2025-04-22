2acd89458b249179b6bd40db0ed8c5777283ffd8
piotrjass <131187708+piotrjass@users.noreply.github.com>
2025-04-22 14:22:14 +0200

    Add startup extensions

---
+++ Startup/Extensions/CorsExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Startup.Extensions;
+
+    public static class CorsExtensions
+    {
+        public static void AddCorsPolicy(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowLocalhost", builder =>
+                {
+                    builder.WithOrigins(configuration["Frontend:Url"]) 
+                        .AllowAnyMethod()
+                        .AllowAnyHeader()
+                        .AllowCredentials();
+                });
+            });
+        }
+    }
